7a78fc7513c7b953c9495a85bf843523950da42f
Andre Thomas <drethedevjs@gmail.com>
2025-04-21 13:02:01 -0400

    Made committee view mobile responsive.

--- src/views/CommitteeView.vue
+++ src/views/CommitteeView.vue
@@ -35,7 +35,7 @@ const members = committeeMembers
     </p>
 
     <h2 class="text-center mb-10 font-bold">Leadership</h2>
-    <div class="grid grid-cols-3 gap-4 place-items-center">
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 place-items-center">
       <FwbCard
         v-for="member in leadership"
         :key="member.id"
