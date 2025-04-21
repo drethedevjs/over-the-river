c1cf82d03566d6a03b0d46e40d7fbf99024f9a5e
Andre Thomas <drethedevjs@gmail.com>
2025-04-21 11:44:13 -0400

    Added safelist and dark colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
     "./node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}",
   ],
+  safelist: [
+    {
+      pattern: /from-(yellow|green|blue|purple|pink)-(400)/,
+    },
+    {
+      pattern: /via-(yellow|green|blue|purple|pink)-(700)/,
+    },
+    {
+      pattern: /to-(yellow|green|blue|purple|pink)-(600)/,
+    },
+  ],
   theme: {
     extend: {
       colors: {
@@ -13,6 +24,12 @@ export default {
         contrast: "#0033A0", // Blue
         neutral: "#101820", // Black
         white: "#FFFFFF", // White
+        // Dark mode variants
+        "primary-dark": "#FFD447",
+        "secondary-dark": "#FF5C5C",
+        "contrast-dark": "#5C88FF",
+        "neutral-dark": "#E4E4E4",
+        "bg-dark": "#0D0D0D",
       },
     },
   },
