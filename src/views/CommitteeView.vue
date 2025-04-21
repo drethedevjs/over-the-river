748be52558009343c6686e01ff62699f1d8bc67e
Andre Thomas <drethedevjs@gmail.com>
2025-04-21 12:47:13 -0400

    Added committee members, added roles, and added leadership section on committee page.

--- src/views/CommitteeView.vue
+++ src/views/CommitteeView.vue
@@ -4,6 +4,24 @@ import PhoneIcon from "@/components/icons/PhoneIcon.vue";
 import committeeMembers from "@/data/committeeMembers";
 import { FwbCard } from "flowbite-vue";
 import { Mail } from "lucide-vue-next";
+
+const leadership = committeeMembers.filter((m) => m.inLeadership);
+const members = committeeMembers
+  .filter((m) => !m.inLeadership)
+  .sort((a, b) => {
+    const nameA = a.lastName.toUpperCase();
+    const nameB = b.lastName.toUpperCase();
+
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  });
 </script>
 
 <template>
@@ -15,15 +33,46 @@ import { Mail } from "lucide-vue-next";
       behind the scenes to make our gathering unforgettable. We're proud to introduce the people
       helping bring our family together — one call, one email, and one memory at a time.
     </p>
-    <div
-      class="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-3 lg:grid-cols-3 place-items-center lg:gap-4 mb-10"
-    >
+
+    <h2 class="text-center mb-10 font-bold">Leadership</h2>
+    <div class="grid grid-cols-3 gap-4 place-items-center">
       <FwbCard
-        v-for="member in committeeMembers"
+        v-for="member in leadership"
         :key="member.id"
         :img-src="member.imgSrc"
         variant="image"
       >
+        <div class="p-5">
+          <h5
+            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white otr-gradient-2 cursor-default"
+          >
+            {{ member.firstName }} {{ member.lastName }}
+          </h5>
+          <p class="font-normal text-gray-700 dark:text-gray-400">
+            {{ member.role }}
+          </p>
+          <div class="flex flex-row gap-4">
+            <a :href="member.facebook" target="_blank" v-if="member.facebook">
+              <FacebookIcon />
+            </a>
+            <a :href="'tel:' + member.phone" v-if="member.phone">
+              <PhoneIcon />
+            </a>
+            <a :href="'mailto:' + member.email" v-if="member.email">
+              <Mail v-if="member.email" class="hover:text-primary" />
+            </a>
+          </div>
+        </div>
+      </FwbCard>
+    </div>
+
+    <hr class="my-10" />
+
+    <h2 class="text-center mb-10 font-bold">Members</h2>
+    <div
+      class="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-3 lg:grid-cols-3 place-items-center lg:gap-4 mb-10"
+    >
+      <FwbCard v-for="member in members" :key="member.id" :img-src="member.imgSrc" variant="image">
         <div class="p-5">
           <h5
             class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white otr-gradient-2 cursor-default"
