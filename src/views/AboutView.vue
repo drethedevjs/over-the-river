b6e33d745f332e1636726a52a143c1626bc70868
Andre Thomas <drethedevjs@gmail.com>
2025-04-21 12:11:03 -0400

    Added "over the river" blurb.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,9 +7,16 @@ import { FwbImg } from "flowbite-vue";
 
     <h2>What is "Over the River"</h2>
     <p>
-      Enim cupidatat ea adipisicing ad Lorem non ut anim sint dolor fugiat. Aliqua culpa laborum
-      consequat aute anim et. Duis aliqua laborum officia velit id cupidatat velit non. Amet amet
-      tempor incididunt elit in incididunt eu laborum qui duis sint ut.
+      In our family, the phrase “over the river” isn't just a geographic reference — it's part of
+      our shared language and history. In earlier generations, our family was rooted in both
+      Cincinnati, Ohio and Covington, Kentucky, two cities separated by the Ohio River.
+    </p>
+    <p>
+      Back in the day, whenever there was a reunion, cookout, or any random family event, the
+      natural question was, “Where's it going to be?” And the answer could be, “Over the river.” The
+      meaning depended on where you were asking from: in Covington, it meant heading to Cincinnati,
+      and in Cincinnati, it meant crossing into Covington. It was a simple phrase that referenced
+      the city of gathering.
     </p>
 
     <h2>Big Grannie / Little Grannie</h2>
